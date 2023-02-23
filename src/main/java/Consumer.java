99b73ac1ca540a84604f53a8e9176ae165bf9a97
kiwi <41132322+whs146@users.noreply.github.com>
2023-02-23 15:00:51 -0800

    Update for more details

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Consumer {
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer {
+
+    public static ConcurrentHashMap<String, HashSet<String>> map = new ConcurrentHashMap<>();
+    final static private int NUMTHREADS = 200;
+    private static String host = "54.244.11.31";
+    public static void main(String[] args) throws Exception {
+
+        Connection connection = Consumer.getConnection();
+        ExecutorService pool = Executors.newFixedThreadPool(NUMTHREADS);
+        for (int i = 0; i < NUMTHREADS; i++) {
+            pool.execute(new MessageDispatcher(connection));
+
+        }
+
+
+    }
+
+
+    public static Connection getConnection() throws IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+
+        factory.setHost(host);
+        factory.setPort(5672);
+
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        Connection connection = factory.newConnection();
+        return connection;
+    }
+
 }
+
--- src/main/java/MessageDispatcher.java
+++ src/main/java/MessageDispatcher.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class MessageDispatcher {
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class MessageDispatcher implements Runnable {
+
+    private Connection connection;
+
+
+    public MessageDispatcher(Connection connection) {
+
+        this.connection = connection;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Channel channel = connection.createChannel();
+            channel.basicConsume("direct2", true, new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    String message = new String(body, "UTF-8");
+                    ConcurrentHashMap<String, HashSet<String>> map = Consumer.map;
+                    int index = message.indexOf("swiper");
+                    int index2 = message.indexOf("swipee");
+                    int index3 = message.indexOf("comment");
+                    String swiper = message.substring(index + 9, index2 - 3);
+                    int index4 = message.indexOf("leftOrRight");
+                    String direction = message.charAt(index4 + 14) == 'r' ? "right" : "left";
+                    if (direction.equals("right")) {
+                        if (!map.containsKey(swiper)) {
+                            map.put(swiper, new HashSet<>(100));
+                        }
+                        if (map.get(swiper).size() < 100) {
+                            map.get(swiper).add(message.substring(index2 + 9, index3 - 3));
+                        }
+
+                    }
+                }
+
+            });
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
+
+
